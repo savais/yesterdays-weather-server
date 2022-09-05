cbe4f48b9303da1faedeb7cc4b072d707971b272
Saku V <saku.vaisanen@student.lab.fi>
2022-09-05 19:45:12 +0300

    added most of the required filters

--- src/filter.js
+++ src/filter.js
@@ -5,17 +5,164 @@
 const paramList = ['tempC', 'windSpeed', 'windDirection', 'cloudCoverage', 'totalCloudCoverage', 'rain_1h']
 
 
-/**
- * filterData
- * Filters data to only include required measurements
- * 
- * @param {object} today
- * @param {object} yesterday
- * @param {object} daybefore
- * 
- * @returns {object} a filtered object containing today, yesterday and daybefore
- */
 
-const filterData = (today, yesterday, daybefore) => {
+const filterDay = (day) => {
+    let result = {};
 
+    for (const [key, value] in day) {
+        switch (key) {
+            
+            case 'rain_1h':
+                result[key] = filterRain(value);
+                break;
+
+            case 'totalCloudCoverage':
+                    
+
+            case 'cloudCoverage':
+                result[key] = filterClouds(value);
+                break;
+
+            case 'windDirection':
+                result[key] = filterWindDirection(value);
+                break;
+
+            case 'windSpeed':
+            case 'tempC':
+                result[key] = filterLinearValue(value);
+                break;
+
+            default:
+                break;
+        }
+
+    }
 }
+
+
+
+/**
+ * filterLinearValue : for filtering linear values like temperature and windspeed
+ * @param {array} arr 
+ * @returns {array} array containing min, max, mean
+ */
+const filterLinearValue = (arr) => {
+    let result;
+
+    result = {...getMinMax(arr.map(e => e.value))}
+    result['mean'] = getMean(getDaytime(arr, 10, 20).map(e => e.value))
+
+    return result
+}
+
+/**
+ * filterWindDirection : boils down directional number (0-360) to 0-7 (wind directions: N, NE, E...)
+ * @param {array} arr array of datapoints {'time': <dateobject>, 'value': <measurement>}
+ * @returns 
+ */
+const filterWindDirection = (arr) => {
+
+    let daytimeArr = getDaytime(arr, 10, 20)
+    arr = daytimeArr.map(e => {return normalizeWindDirection(e.value)})
+
+    return getMode(arr)
+}
+
+/**
+ * filterRain : takes in a days worth of datapoints in an array, processes them down to few numbers.
+ * @param {array} arr array of datapoints {'time': <dateobject>, 'value': <measurement>}
+ * @returns {object} with mean, max, total  rain fall
+ */
+const filterRain = (arr) => {
+    let result = {};
+
+    result['mean'] = getMean(getDaytime(arr, 10, 20).map(e => e.value))
+    result['max'] = Math.max(...getDaytime(arr, 10, 20).map(e => e.value))
+    result['total'] = getSum(arr.map(e => e.value))
+
+    return result;
+}
+
+
+const filterClouds = (arr) => {
+    let result = {};
+
+    result['mean'] = getMean(getDaytime(arr, 10, 20).map(e => e.value))
+    result['mode'] = getMode(getDaytime(arr, 10, 20).map(e => e.value))
+
+    return result;
+}
+
+
+/**
+ * Filtering datapoints between given hours
+ * @param {array} arr array of datapoints {'time': <dateobject>, 'value': <measurement>}
+ * @param {int} start in hours from 0:00, ie 10 -> 10:00
+ * @param {int} stop  in hours from 0:00, ie 20 -> 20:00
+ * @returns {array} an array of datapoints between the given hours
+ */
+ const getDaytime = (arr, start, stop) => {
+    let startDate = new Date(arr[0].time.getTime()).zeroHours().addHours(start)
+    let stopDate = new Date(arr[0].time.getTime()).zeroHours().addHours(stop)
+
+    return arr.filter(e => (e.time >= startDate && e.time <= stopDate))
+}
+
+
+const normalizeClouds = (x) => {return Math.round((8)*(x/100))}
+
+const normalizeWindDirection = (x) => {
+    let n = Math.round((8)*(x/360))
+    return n === 8 ? 0 : n
+    }
+
+
+
+
+// Helpers -------------------------------------------------------------------------
+Date.prototype.addHours = function(h) {
+    this.setTime(this.getTime() + (h*60*60*1000));
+    return this;
+  }
+  
+Date.prototype.zeroHours = function() {
+    this.setHours(0);
+    this.setMinutes(0);
+    this.setSeconds(0);
+    this.setMilliseconds(0);
+
+    return this;
+}
+
+
+const getMinMax = (arr) => {
+
+    return {'min': Math.min(arr), 'max': Math.max(arr) }
+}
+
+
+const getMean = (arr) => {
+    let sum = 0;
+
+    arr.forEach(e => sum+=e)
+
+    return sum/arr.length
+}
+
+const getMode = (arr) => {
+    let modeArr = arr.map((e) => {return arr.filter(elm => e===elm).length})
+
+
+
+    return arr[modeArr.indexOf(Math.max(...modeArr))]
+}
+
+const getSum = (arr) => {
+    let sum = 0;
+    arr.forEach(e => {sum += e})
+
+    return sum;
+}
+
+
+export { filterDay, filterRain, filterWindDirection, getMode}
--- test/filter.spec.js
+++ test/filter.spec.js
@@ -1,5 +1,5 @@
 import { assert, expect, should } from 'chai';
-import { filterData } from '../src/filter.js';
+import { filterDay, filterRain, filterWindDirection, getMode } from '../src/filter.js';
 
 const testData = {}
 
@@ -7,10 +7,152 @@ const testData = {}
 describe('Filter.js', () => {
     describe('filterData()', () => {
         it('Should return only relevant bits of data', () => {
-            const filteredData = filterData(testData, testData, testData);
-
-            expect(filteredData).to.have.all.keys('today', 'yesterday', 'daybefore');
-            expect(filteredData.today).to.have.all.keys('tempC','windSpeed','windDirection','cloudCoverage','rain_1h');
+            
         })
+    })
+
+
+    describe('getMode()', () => {
+        it('Should return mode of an array of numbers', () => {
+            let numbers = [1,2,3,3,3,5]
+
+            expect(getMode(numbers)).to.equal(3)
+
+        })
+    })
+
+    describe('filterWindDirection()', () => {
+        it('Should return a number between 1-8', () => {
+            let testData = [
+                {
+                  "time": new Date("2022-09-03T21:00:00.000Z"),
+                  "value": 322
+                },
+                {
+                  "time": new Date("2022-09-03T22:00:00.000Z"),
+                  "value": 326
+                },
+                {
+                  "time": new Date("2022-09-03T23:00:00.000Z"),
+                  "value": 328
+                },
+                {
+                  "time": new Date("2022-09-04T00:00:00.000Z"),
+                  "value": 328
+                },
+                {
+                  "time": new Date("2022-09-04T01:00:00.000Z"),
+                  "value": 327
+                },
+                {
+                  "time": new Date("2022-09-04T02:00:00.000Z"),
+                  "value": 326
+                },
+                {
+                  "time": new Date("2022-09-04T03:00:00.000Z"),
+                  "value": 330
+                },
+                {
+                  "time": new Date("2022-09-04T04:00:00.000Z"),
+                  "value": 327
+                },
+                {
+                  "time": new Date("2022-09-04T05:00:00.000Z"),
+                  "value": 329
+                },
+                {
+                  "time": new Date("2022-09-04T06:00:00.000Z"),
+                  "value": 330
+                },
+                {
+                  "time": new Date("2022-09-04T07:00:00.000Z"),
+                  "value": 342
+                },
+                {
+                  "time": new Date("2022-09-04T08:00:00.000Z"),
+                  "value": 331
+                },
+                {
+                  "time": new Date("2022-09-04T09:00:00.000Z"),
+                  "value": 338
+                },
+                {
+                  "time": new Date("2022-09-04T10:00:00.000Z"),
+                  "value": 342
+                },
+                {
+                  "time": new Date("2022-09-04T11:00:00.000Z"),
+                  "value": 332
+                },
+                {
+                  "time": new Date("2022-09-04T12:00:00.000Z"),
+                  "value": 329
+                },
+                {
+                  "time": new Date("2022-09-04T13:00:00.000Z"),
+                  "value": 347
+                },
+                {
+                  "time": new Date("2022-09-04T14:00:00.000Z"),
+                  "value": 351
+                },
+                {
+                  "time": new Date("2022-09-04T15:00:00.000Z"),
+                  "value": 346
+                },
+                {
+                  "time": new Date("2022-09-04T16:00:00.000Z"),
+                  "value": 22
+                },
+                {
+                  "time": new Date("2022-09-04T17:00:00.000Z"),
+                  "value": 33
+                },
+                {
+                  "time": new Date("2022-09-04T18:00:00.000Z"),
+                  "value": 53
+                },
+                {
+                  "time": new Date("2022-09-04T19:00:00.000Z"),
+                  "value": 16
+                },
+                {
+                  "time": new Date("2022-09-04T20:00:00.000Z"),
+                  "value": 25
+                }
+              ]
+
+            let result = filterWindDirection(testData)
+
+            expect(result).to.be.within(0,8)
+        })
+    })
+
+    describe('filterRain()', () => {
+        it('Should return an object with (max, mean, total) rain fall.', () => {
+            let testData = [{
+                "time": new Date("2022-09-03T21:00:00.000Z"),
+                "value": 20
+              },
+              {
+                "time": new Date("2022-09-04T10:00:00.000Z"),
+                "value": 1
+              },
+              {
+                "time": new Date("2022-09-04T11:00:00.000Z"),
+                "value": 1
+              },
+              {
+                "time": new Date("2022-09-04T12:00:00.000Z"),
+                "value": 10
+            }]
+
+            let result = filterRain(testData)
+
+            expect(result).to.be.a('object')
+            expect(result.mean).to.be.below(result.max)
+            expect(result.total).to.be.at.least(result.max)
+        })
+
     })
 })
